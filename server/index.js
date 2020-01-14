e4460882eec03b4f00d8f3e2ced851efda0eba32
jonathanhkim <jhkim792@gmail.com>
2020-01-13 16:53:30 -0800

    set up endpoints

--- server/index.js
+++ server/index.js
@@ -1,7 +1,20 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3001
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(bodyParser.json());
+
+app.get('/finishGame', (req, res) => {
+  let data = { test: 'asdf'};
+  res.status(200);
+  res.send(JSON.stringify(data));
+})
+
+app.get('/getText', (req, res) => {
+  let data = {};
+  res.status(200);
+
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
